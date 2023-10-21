f038ac868481706c1cf3b61b01aabf1c20710ec5
Satyam Pathania <71765680+Satyampathania@users.noreply.github.com>
2023-10-21 13:54:29 +0530

    Update password encryption program.py
    just updated with all the links or mini  compling tools included , merger this asap

--- password encryption program.py
+++ password encryption program.py
@@ -1,9 +1,49 @@
-import base64
+'''Encrypting hashes doesn't make sense in the traditional sense, as hashes are meant to be one-way functions. Hashing is used to transform data into a fixed-size string of characters, which is useful for data integrity checks and password storage. Encryption, on the other hand, is a two-way process that can be reversed with the right key.
 
-def encrypt_pass(password):
-    encoded_bytes = base64.b64encode(password.encode())
-    print(encoded_bytes)
-user_pass =input("Enter your password :")
-encrypt_pass(user_pass)
+If you want to securely store sensitive information like passwords, you should use a secure hashing algorithm, like bcrypt or Argon2, and store the hash value, not the original password. Here's a simple example in Python using the bcrypt library to hash and verify passwords:
 
+    Install the bcrypt library if you haven't already. You can install it using pip:
 
+bash
+
+pip install bcrypt
+
+    Now, let's create a Python tool to hash and verify passwords:
+
+python'''
+
+import bcrypt
+
+def hash_password(password):
+    # Generate a salt and hash the password
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed
+
+def verify_password(hashed_password, input_password):
+    # Check if the input password matches the stored hash
+    return bcrypt.checkpw(input_password.encode('utf-8'), hashed_password)
+
+if __name__ == '__main__':
+    # Example usage
+    password = "my_secret_password"
+
+    # Hash the password
+    hashed_password = hash_password(password)
+    print("Hashed Password:", hashed_password)
+
+    # Verify a password
+    input_password = "my_secret_password"
+    is_valid = verify_password(hashed_password, input_password)
+
+    if is_valid:
+        print("Password is valid.")
+    else:
+        print("Password is invalid.")
+
+'''In this code:
+
+    hash_password function generates a random salt and uses it to hash the provided password.
+    verify_password function takes a stored hash and an input password, then checks if the input password matches the stored hash.
+
+Remember to store and compare the hashed password values instead of the original passwords. This approach ensures that passwords are securely stored and verified without the need for decryption.'''
